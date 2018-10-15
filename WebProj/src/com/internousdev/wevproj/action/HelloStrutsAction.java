9d1b1a3e8928518f8e377cc6879d49111965f27e
hiroaki-shishido <kaesaru2989@gmail.com>
2018-10-15 21:34:21 +0900

    20181015

---
+++ WebProj/src/com/internousdev/wevproj/action/HelloStrutsAction.java
@@ -0,0 +1,9 @@
+package com.internousdev.wevproj.action;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloStrutsAction extends ActionSupport {
+	public String execute(){
+		return SUCCESS;
+	}
+
+}
